ecb5807c0891e1898e69e78bcbfcb27ef44d99ee
Sunghum Paik (Brian) <zerosial@gmail.com>
2023-12-28 15:21:44 +0900

    feat: className 으로 수정

--- src/Components/LoadingSkeletonBox.js
+++ src/Components/LoadingSkeletonBox.js
@@ -1,13 +1,13 @@
 export const LoadingSkeletonBox = () => {
   return (
     <div className="w-full p-4">
-      <div class="mx-auto w-full max-w-md rounded-2xl bg-white">
+      <div className="mx-auto w-full max-w-md rounded-2xl bg-white">
         <div className="className=flex w-full justify-between rounded-lg  bg-blue-100 px-4 py-4 my-4 text-left text-sm font-medium text-blue-900">
-          <div class="animate-pulse flex space-x-4">
-            <div class="flex-1 space-y-6 py-1">
-              <div class="h-3 bg-slate-700 rounded"></div>
-              <div class="space-y-4">
-                <div class="h-3 bg-slate-700 rounded"></div>
+          <div className="animate-pulse flex space-x-4">
+            <div className="flex-1 space-y-6 py-1">
+              <div className="h-3 bg-slate-700 rounded"></div>
+              <div className="space-y-4">
+                <div className="h-3 bg-slate-700 rounded"></div>
               </div>
             </div>
           </div>
