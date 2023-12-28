87efec1962f0763ab3d8c1fb3359b2939fa51471
Sunghum Paik (Brian) <zerosial@gmail.com>
2023-12-28 10:06:17 +0900

    feat: 간단한 유저 체크 기능 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,75 @@
-import { Bars4Icon } from "@heroicons/react/20/solid";
+import { Menu, Transition } from "@headlessui/react";
+import { UserIcon } from "@heroicons/react/20/solid";
+import { Fragment, useState } from "react";
 
 export const Header = () => {
+  const [userName, setUserName] = useState("");
   return (
     <div className="bg-blue-600 text-white p-4">
       <div className="flex justify-between items-center">
         <div className="text-white" variant="ghost">
           1:1문의하기
         </div>
-        <Bars4Icon className="text-white w-5 h-5" />
+        <div className="flex">
+          <div className="p-2">{userName}</div>
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="inline-flex w-full justify-center rounded-md  px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+                <UserIcon className="text-white w-6 h-6" />
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        onClick={() => setUserName("테스트")}
+                      >
+                        생성
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        로그인
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className="px-1 py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        로그아웃
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
       </div>
     </div>
   );
