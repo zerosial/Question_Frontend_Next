6f1474b155084ce46059eaea92066ee7842c7a72
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-01-02 13:10:13 +0900

    refactor: div 변경, 미로그인시 컴포넌트 추가

--- src/Page/question/ListQuestionPage.js
+++ src/Page/question/ListQuestionPage.js
@@ -2,14 +2,26 @@ import { GETDisclosureItems } from "api/apiService";
 import { DisclosureItem } from "./Components/DisclosureItem";
 import { useQuery } from "@tanstack/react-query";
 import { LoadingSkeletonBox } from "Components/LoadingSkeletonBox";
+import { useSyncedEmailStore } from "store/useSyncedEmailStore";
 
 export const ListQuestionPage = () => {
+  const { email } = useSyncedEmailStore();
   const { isFetching, data } = useQuery({
     queryKey: ["questionList"],
     queryFn: GETDisclosureItems,
   });
 
   if (isFetching) return <LoadingSkeletonBox />;
+
+  if (!email) {
+    return (
+      <div className="w-full p-8">
+        <div className="mx-auto w-full max-w-md rounded-2xl bg-white">
+          로그인 하여 내역을 확인해 주세요.
+        </div>
+      </div>
+    );
+  }
 
   if (data.length === 0) {
     return (
