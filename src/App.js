aa117ba4091eef6c27bf7563fe1f5e5ab3471938
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-01-02 09:51:20 +0900

    feat: 해더 파일명 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Header } from "./Components/Header/Header";
+import { Header } from "./Components/Header/index.js";
 import { QuestionLayout } from "./Page/question/QuestionLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
